03b7cb92558e0b3a56043e21bc7430840fab31d8
josalza902 <131299934+josalza902@users.noreply.github.com>
2025-08-26 13:58:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import conectarMongo from './conectmongo.js';
+import { ObjectId } from 'mongodb';
+import userRoute from './user-route.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.static("/login.html"))
+
+app.use('/api', userRoute);
+app.listen(
+    {hostname:process.env.APP_HOSTNAME, port:process.env.APP_PORT},
+    ()=>{
+        console.log(`Server listening on ${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`);
+    }
+)
